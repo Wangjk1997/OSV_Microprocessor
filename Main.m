dcc5011be96b856ab4f717e56a5dda05d32dd70c
wangjk1997 <wangjk@gatech.edu>
2021-02-28 21:36:34 -0500

    log

--- Main.m
+++ Main.m
@@ -6,19 +6,28 @@ addpath('.\eventHandler');
 global duration;
 global stateHistory;
 global rawdataHistory;
+global fHistory;
+global duty_cyclesHistory;
 
 stateHistory = [];
 rawdataHistory = [];
+fHistory = [];
+duty_cyclesHistory = [];
 duration = 0.2;
-time = 15;
+time = 35;
 
 t = timer;
 t.ExecutionMode = 'fixedRate';
-t.Period = 0.2;
+t.Period = duration;
 t.TasksToExecute = time/duration;
 t.TimerFcn = @event_handler;
 % t.TimerFcn = @event_handler_test;
 start(t);
+
+tmp = clock;
+filename = ".\data\workspace" +num2str(tmp(2))+num2str(tmp(3))+num2str(tmp(4))+num2str(tmp(5));
+save(filename);
+disp('Control End' )
 
 
 
--- eventHandler/event_handler.m
+++ eventHandler/event_handler.m
@@ -2,6 +2,8 @@ function event_handler(~,~)
     global duration;
     global stateHistory;
     global rawdataHistory;
+    global fHistory;
+    global duty_cyclesHistory;
     
     persistent port;
     persistent currentState;
@@ -18,7 +20,7 @@ function event_handler(~,~)
     %send command and receivie GPS data
     %initialize first command
     if(isempty(command_string))
-        command_string = '$01,SETM,355,355,355,355,355,355';
+        command_string = '$01,SETM,255,255,255,255,255,255';
         port = serialport("COM3",230400);
         return;
     end
@@ -38,19 +40,19 @@ function event_handler(~,~)
     if(isempty(currentState))
         currentState = RigidBodyState_plane(duration, px_left, py_left, px_right, py_right);
         %initialize reference
-        %ref_px_bn_n = currentState.p_bn_n(1);
-        %ref_py_bn_n = currentState.p_bn_n(2);
-        %ref_yaw = currentState.psi;
-        ref_px_bn_n = 0.5;
-        ref_py_bn_n = 1;
-        ref_yaw = 0;
+        ref_px_bn_n = currentState.p_bn_n(1);
+        ref_py_bn_n = currentState.p_bn_n(2) + 2;
+        ref_yaw = currentState.psi;
+%         ref_px_bn_n = 0.5;
+%         ref_py_bn_n = 1;
+%         ref_yaw = 0;
         
         %initialize PID parameters and controllers
-        kp_px_bn_n = 0.2;
+        kp_px_bn_n = 0.02;
         ki_px_bn_n = 0;
         kd_px_bn_n = 0;
         
-        kp_py_bn_n = 0.2;
+        kp_py_bn_n = 0.02;
         ki_py_bn_n = 0;
         kd_py_bn_n = 0;
         
@@ -81,9 +83,9 @@ function event_handler(~,~)
     [pid_px_bn_n, ux] = pid_px_bn_n.calculate(ref_px_bn_n - currentState.p_bn_n(1));
     [pid_py_bn_n, uy] = pid_py_bn_n.calculate(ref_py_bn_n - currentState.p_bn_n(2));
     [pid_yaw, tz] = pid_yaw.calculate((ref_yaw - currentState.psi) * 180/pi);
-    currentState.p_bn_n(1);
-    currentState.p_bn_n(2);
-    currentState.psi;
+    X = currentState.p_bn_n(1);
+    Y = currentState.p_bn_n(2);
+    PSI = currentState.psi;
     
     % convert force and torque to cmd
     user_tau = [ux;uy;0;0;0;tz];
@@ -92,7 +94,10 @@ function event_handler(~,~)
     duty_cycles = thrust2dutyCycle(f);
     duty_cycles = duty_cycle_saturation(duty_cycles);
     command_string = convertCMD(duty_cycles);
-%     
+    fHistory = [fHistory, f];
+    duty_cyclesHistory = [duty_cyclesHistory, duty_cycles];
+    
+    
     % plot real time positions
     addpoints(animation_frame_left, px_left, py_left);
     addpoints(animation_frame_right, px_right, py_right);
